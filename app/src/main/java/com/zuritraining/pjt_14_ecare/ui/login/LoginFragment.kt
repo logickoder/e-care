f6f51f4ae5d7c22c02712d4a960188f71336c3a2
Jeffrey Orazulike <chukwudumebiorazulike@gmail.com>
2021-06-25 12:26:12 +0100

    Designed the Appointments layout pages including the adapter and items for the 2 different tabs in the page

--- app/src/main/java/com/zuritraining/pjt_14_ecare/ui/login/LoginFragment.kt
+++ app/src/main/java/com/zuritraining/pjt_14_ecare/ui/login/LoginFragment.kt
@@ -1,11 +1,11 @@
 package com.zuritraining.pjt_14_ecare.ui.login
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.zuritraining.pjt_14_ecare.R
 import com.zuritraining.pjt_14_ecare.databinding.FragmentLoginBinding
@@ -27,7 +27,7 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.loginBtn.setOnClickListener {
+        binding.buttonLogin.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_nav_home)
         }
     }
---
+++ app/src/main/java/com/zuritraining/pjt_14_ecare/ui/patient/appointments/AppointmentsFragment.kt
@@ -0,0 +1,38 @@
+package com.zuritraining.pjt_14_ecare.ui.patient.appointments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayout
+import com.zuritraining.pjt_14_ecare.databinding.FragmentAppointmentsBinding
+
+/**
+ * @author Jeffrey Orazulike [chukwudumebiorazulike@gmail.com]
+ * Created 24-Jun-21 at 9:57 PM
+ */
+class AppointmentsFragment : Fragment(){
+    private lateinit var binding: FragmentAppointmentsBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentAppointmentsBinding.inflate(inflater, container, false)
+        binding.recyclerviewAppointments.adapter = AppointmentsAdapter.UpcomingAppointmentsAdapter()
+        binding.recyclerviewAppointments.layoutManager = LinearLayoutManager(requireContext())
+        binding.tabsAppointments.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                tab?.let {
+                    when(it.position){
+                        0 -> binding.recyclerviewAppointments.adapter = AppointmentsAdapter.UpcomingAppointmentsAdapter()
+                        1 -> binding.recyclerviewAppointments.adapter = AppointmentsAdapter.HistoryAppointmentsAdapter()
+                    }
+                    binding.recyclerviewAppointments.adapter?.notifyDataSetChanged()
+                }
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+        return binding.root
+    }
+}
